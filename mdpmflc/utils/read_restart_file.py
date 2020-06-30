24f995b752e1903762e81692865a53e946e707b0
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-06-30 01:35:56 +0100

    Restructure the .restart file reader, and read the rest of the header
    Now resolves https://github.com/jftsang/mdpmflc/issues/12, and is ready
    to be extended to read other parts of the .restart file.
    I think it will be better in the long run to read the entire .restart
    file, rather than the .data and .fstat files. It isn't necessarily any
    slower, either, since we can seek based on the number of lines.

--- mdpmflc/utils/read_restart_file.py
+++ mdpmflc/utils/read_restart_file.py
@@ -5,12 +5,26 @@ import linecache  # for reading large files
 def read_restart_file(restart_fn, header_only=False):
     """Reads and parses a .restart file."""
     dic = dict()
+    dic.update(read_restart_file_header(restart_fn))
+    return dic
+
+
+def read_restart_file_header(restart_fn):
+    dic = dict()
 
     dataFile_line = linecache.getline(restart_fn, 2).strip().split()
     dic['dataFileCounter'] = int(dataFile_line[8])
 
     fStatFile_line = linecache.getline(restart_fn, 3).strip().split()
     dic['fStatFileCounter'] = int(fStatFile_line[8])
+
+    domain_line = linecache.getline(restart_fn, 8).strip().split()
+    dic['xMin'] = float(domain_line[1])
+    dic['xMax'] = float(domain_line[3])
+    dic['yMin'] = float(domain_line[5])
+    dic['yMax'] = float(domain_line[7])
+    dic['zMin'] = float(domain_line[9])
+    dic['zMax'] = float(domain_line[11])
 
     time_line = linecache.getline(restart_fn, 9).strip().split()
     dic['timeStep'] = float(time_line[1])
@@ -20,5 +34,10 @@ def read_restart_file(restart_fn, header_only=False):
     dic['ntimeStepsMax'] = int(dic['timeMax'] / dic['timeStep'])
 
     dic['progress'] = dic['time'] / dic['timeMax']
+
+    misc_line = linecache.getline(restart_fn, 10).strip().split()
+    dic['gx'] = float(misc_line[5])
+    dic['gy'] = float(misc_line[6])
+    dic['gz'] = float(misc_line[7])
 
     return dic
