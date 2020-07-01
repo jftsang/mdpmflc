423eee407802c334a5b47ca5c0b01ff9f1f2063d
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-07-02 00:40:25 +0100

    [BUGFIX] The last commit broke everything

--- mdpmflc/controller/miscPages.py
+++ mdpmflc/controller/miscPages.py
@@ -13,13 +13,3 @@ def mainpage():
                            DPMDIR=DPMDIR,
                            available_series=get_available_series(),
                            available_drivers=DPMDRIVERS)
-
-
-@app.route('/style.css')
-def stylesheet():
-    return app.send_static_file("style.css")
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return app.send_static_file("favicon.ico")
--- mdpmflc/mdpmflc.py
+++ mdpmflc/mdpmflc.py
@@ -6,15 +6,17 @@ from mdpmflc import app
 # Utilities
 
 # Controllers
-import mdpmflc.controller.miscPages
-import mdpmflc.controller.driver.viewDriver
+import mdpmflc.controller.driver.view_driver
 import mdpmflc.controller.job.start_job
 import mdpmflc.controller.results.simulation
 import mdpmflc.controller.results.listing
 import mdpmflc.controller.results.plots
 import mdpmflc.controller.results.raw
+import mdpmflc.controller.help
+import mdpmflc.controller.miscPages
 
-# Error handlers
+# Helpers
+import mdpmflc.static
 import mdpmflc.errorhandlers
 
 def start_app():
