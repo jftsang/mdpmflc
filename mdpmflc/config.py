4db622263aaf23cd96dee24b167616296d8e0385
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-07-02 01:47:36 +0100

    [WIP] first steps towards jobs database

--- mdpmflc/config.py
+++ mdpmflc/config.py
@@ -1,3 +1,8 @@
+import os
+
+
 DPMDIR = "/Volumes/Transcend/MercuryDPM/MercuryBuild/Drivers/USER/jmft2/Fingering"
 SRCDIR = "/Volumes/Transcend/MercuryDPM/MercurySource/Drivers/USER/jmft2/Fingering"
 DPMDRIVERS = ["MaserFingering"]
+
+SQLITE_FILE = os.path.join(DPMDIR, "mdpmflc.sqlite")
--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(name='mdpmflc',
       url='https://www.github.com/jftsang/mdpmflc/',
       packages=find_packages(),
       install_requires=['Flask>=1.1.2',
-                        'matplotlib>=3.2.0']
+                        'matplotlib>=3.2.0',
+                        'Flask-sqlalchemy>=2.4.3']
  )
