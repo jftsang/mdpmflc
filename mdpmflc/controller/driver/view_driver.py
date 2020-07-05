9354f25f7acbaac6e2a07940873eba5f232ee71d
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-07-05 18:38:07 +0100

    More instructions about how to use .config files
    The source listings page now also supports the ability to show an
    .example.config file, if one is available in the SRCDIR. The required
    fields in such a file are now also shown on the page.

--- mdpmflc/controller/driver/view_driver.py
+++ mdpmflc/controller/driver/view_driver.py
@@ -3,6 +3,7 @@ import flask
 from flask import Response, render_template
 
 from mdpmflc import SRCDIR, app
+from mdpmflc.utils.driver import get_config_fields
 from mdpmflc.utils.listings import get_available_series
 
 
@@ -19,11 +20,25 @@ def driverpage(dri):
 @app.route("/driver/<dri>/source")
 def driver_source(dri):
     """A page that shows a driver's source code."""
+    # Read the source file
     src_fn = os.path.join(SRCDIR, dri + ".cpp")
     src_f = open(src_fn, "r")
+    src = src_f.read()
+
+    # Read the example config, if it is available
+    example_config_fn = os.path.join(SRCDIR, dri + ".example.config")
+    try:
+        example_config_f = open(example_config_fn, "r")
+        example_config = example_config_f.read()
+    except OSError:
+        example_config = None
+
+    pars_fields = get_config_fields(src, "pars")
     return render_template("driver_src.html",
                            driver=dri,
-                           src=src_f.read())
+                           src=src,
+                           cfgex=example_config,
+                           pars_fields=pars_fields)
 
 
 @app.route("/driver/<dri>/source/raw")
@@ -32,3 +47,16 @@ def driver_source_raw(dri):
     src_fn = os.path.join(SRCDIR, dri + ".cpp")
     src_f = open(src_fn, "r")
     return Response(src_f.read(), mimetype='text/plain')
+
+
+@app.route("/driver/<dri>/exampleconfig")
+def driver_source_exampleconfig(dri):
+    """Return the example config file."""
+    example_config_fn = os.path.join(SRCDIR, dri + ".example.config")
+    try:
+        example_config_f = open(example_config_fn, "r")
+        example_config = example_config_f.read()
+        return Response(example_config, mimetype='text/plain')
+    except OSError:
+        return Response(404)
+
---
+++ mdpmflc/utils/driver.py
@@ -0,0 +1,19 @@
+import re
+
+
+def get_config_fields(src, pars_name="pars"):
+    """Reads a source code (of a MDPM driver) and find all instances of
+    'pars.at("...")', to find out all the config fields that can be read
+    from a config file.
+    """
+    # regex = f'{pars_name}.at(".*")'
+    regex = r'pars\.at\("(.*?)"\)'
+    matches = re.finditer(regex, src, re.MULTILINE)
+
+    pars_fields = []
+    for matchNum, match in enumerate(matches, start=1):
+        for group_ind in range(1, len(match.groups())+1, 2):
+            if match.group(group_ind) not in pars_fields:
+                pars_fields.append(match.group(group_ind))
+
+    return pars_fields
