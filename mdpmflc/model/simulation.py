5144d78a3a5a7b8393f2fa9c2e1e43b5f6975130
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2020-07-20 01:21:38 +0100

    [BUGFIX] simulation.py counting indices now won't die on unparseable filenames

--- mdpmflc/model/simulation.py
+++ mdpmflc/model/simulation.py
@@ -42,6 +42,6 @@ class Simulation:
         """
         files = os.listdir(self.simdir())
         files_parsed = [f.split(".") for f in files]
-        max_data_index = max([int(fp[2]) for fp in files_parsed if fp[1] == "data"])
-        max_fstat_index = max([int(fp[2]) for fp in files_parsed if fp[1] == "fstat"])
+        max_data_index = max([int(fp[2]) for fp in files_parsed if len(fp) == 3 and fp[1] == "data"])
+        max_fstat_index = max([int(fp[2]) for fp in files_parsed if len(fp) == 3 and fp[1] == "fstat"])
         return files_parsed, max_data_index, max_fstat_index
