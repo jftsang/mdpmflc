20e77160ee2d95b90eb75d6b1d16f3072fb51615
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2021-09-16 13:05:59 +0100

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-setup(name='mdpmflc',
-      version='0.0.0-DEVELOPMENT',
-      description='MercuryDPM Flask Controller',
-      author='J. M. F. Tsang',
-      author_email='j.m.f.tsang@cantab.net',
-      url='https://www.github.com/jftsang/mdpmflc/',
-      packages=find_packages(),
-      install_requires=['Flask>=1.1.2',
-                        'matplotlib>=3.2.0',
-                        'Flask-sqlalchemy>=2.4.3',
-                        'moviepy>=1.0.3',
-                        'numpy>=1.19.4',
-                        'pandas>=1.1.4']
- )
+setup(
+    name='mdpmflc',
+    version='0.0.0-DEVELOPMENT',
+    description='MercuryDPM Flask Controller',
+    author='J. M. F. Tsang',
+    author_email='j.m.f.tsang@cantab.net',
+    url='https://www.github.com/jftsang/mdpmflc/',
+    packages=find_packages(),
+    install_requires=[
+        'Flask>=1.1.2',
+        'matplotlib>=3.2.0',
+        'Flask-sqlalchemy>=2.4.3',
+        'diskcache>=5.2.1',
+        'moviepy>=1.0.3',
+        'numpy>=1.19.4',
+        'pandas>=1.1.4',
+    ]
+)
