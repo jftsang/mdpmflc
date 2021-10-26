af6ba9f3761f15ae541b40f51efeb94bbc0ab368
J. M. F. Tsang <j.m.f.tsang@cantab.net>
2021-10-26 01:19:50 +0100

    Faster to read header of a restart file directly rather than using linecache

--- mdpmflc/utils/read_file.py
+++ mdpmflc/utils/read_file.py
@@ -1,9 +1,10 @@
 from collections import namedtuple
 import os
-import random
-import linecache  # for reading large files
 import pandas as pd
 # https://stackoverflow.com/questions/2081836/reading-specific-lines-only
+from werkzeug.datastructures import ImmutableDict
+
+from mdpmflc.utils.decorators import timed
 
 
 def read_ene_file(ene_fn):
@@ -11,51 +12,58 @@ def read_ene_file(ene_fn):
     return pd.read_csv(ene_fn, sep='\s+')
 
 
-def read_restart_file(restart_fn, header_only=False):
+def read_restart_file(restart_fn, header_only=False) -> ImmutableDict:
     """Reads and parses a .restart file. Raises FileNotFoundError if
     the file doesn't exist
     """
     if not os.path.isfile(restart_fn):
         raise FileNotFoundError(f"{restart_fn} does not exist.")
+    return read_restart_file_header(restart_fn)
+
+
+@timed("Reading header of {restart_fn}")
+def read_restart_file_header(restart_fn) -> ImmutableDict:
     dic = dict()
-    dic.update(read_restart_file_header(restart_fn))
-    return dic
+    with open(restart_fn, 'r') as restart:
+        restart.readline()
 
+        dataFile_line = restart.readline().strip().split()
+        dic['dataFileSaveCount'] = int(dataFile_line[6])
+        dic['dataFileCounter'] = int(dataFile_line[8])
 
-def read_restart_file_header(restart_fn):
-    dic = dict()
+        fStatFile_line = restart.readline().strip().split()
+        dic['fStatFileSaveCount'] = int(fStatFile_line[6])
+        dic['fStatFileCounter'] = int(fStatFile_line[8])
 
-    dataFile_line = linecache.getline(restart_fn, 2).strip().split()
-    dic['dataFileSaveCount'] = int(dataFile_line[6])
-    dic['dataFileCounter'] = int(dataFile_line[8])
+        restart.readline()
+        restart.readline()
+        restart.readline()
+        restart.readline()
 
-    fStatFile_line = linecache.getline(restart_fn, 3).strip().split()
-    dic['fStatFileSaveCount'] = int(fStatFile_line[6])
-    dic['fStatFileCounter'] = int(fStatFile_line[8])
+        domain_line = restart.readline().strip().split()
+        dic['xMin'] = float(domain_line[1])
+        dic['xMax'] = float(domain_line[3])
+        dic['yMin'] = float(domain_line[5])
+        dic['yMax'] = float(domain_line[7])
+        dic['zMin'] = float(domain_line[9])
+        dic['zMax'] = float(domain_line[11])
 
-    domain_line = linecache.getline(restart_fn, 8).strip().split()
-    dic['xMin'] = float(domain_line[1])
-    dic['xMax'] = float(domain_line[3])
-    dic['yMin'] = float(domain_line[5])
-    dic['yMax'] = float(domain_line[7])
-    dic['zMin'] = float(domain_line[9])
-    dic['zMax'] = float(domain_line[11])
+        time_line = restart.readline().strip().split()
+        dic['timeStep'] = float(time_line[1])
+        dic['time'] = float(time_line[3])
+        dic['ntimeSteps'] = int(time_line[5])
+        dic['timeMax'] = float(time_line[7])
+        dic['ntimeStepsMax'] = int(dic['timeMax'] / dic['timeStep'])
 
-    time_line = linecache.getline(restart_fn, 9).strip().split()
-    dic['timeStep'] = float(time_line[1])
-    dic['time'] = float(time_line[3])
-    dic['ntimeSteps'] = int(time_line[5])
-    dic['timeMax'] = float(time_line[7])
-    dic['ntimeStepsMax'] = int(dic['timeMax'] / dic['timeStep'])
+        dic['progress'] = dic['time'] / dic['timeMax']
 
-    dic['progress'] = dic['time'] / dic['timeMax']
+        misc_line = restart.readline().strip().split()
+        dic['gx'] = float(misc_line[5])
+        dic['gy'] = float(misc_line[6])
+        dic['gz'] = float(misc_line[7])
 
-    misc_line = linecache.getline(restart_fn, 10).strip().split()
-    dic['gx'] = float(misc_line[5])
-    dic['gy'] = float(misc_line[6])
-    dic['gz'] = float(misc_line[7])
+    return ImmutableDict(dic)
 
-    return dic
 
 DataFile = namedtuple(
     'DataFile',
